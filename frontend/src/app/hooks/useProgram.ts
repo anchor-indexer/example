bf4ef788883ede36bd54cd061c1f198aa833de23
Mitchel Kelonye <vbstreetz@gmail.com>
2023-05-29 12:55:58 +0400

    fix: useProgram

--- frontend/src/app/hooks/useProgram.ts
+++ frontend/src/app/hooks/useProgram.ts
@@ -1,27 +1,18 @@
-import { useEffect, useState } from 'react';
+import { useMemo } from 'react';
 import * as anchor from '@project-serum/anchor';
 
 import { useProvider } from '@app/contexts/provider';
 
 function useProgram(programId: anchor.web3.PublicKey, idl: anchor.Idl) {
-  const [program, setProgram] = useState<anchor.Program | null>(null);
   const provider = useProvider();
 
-  useEffect(() => {
-    if (!provider) return;
-
-    async function load() {
-      const program = new anchor.Program(
-        idl as anchor.Idl,
-        programId,
-        provider!
-      );
-
-      setProgram(program);
-    }
-
-    load();
-  }, [provider, programId, idl]);
+  const program = useMemo(
+    () =>
+      !provider
+        ? null
+        : new anchor.Program(idl as anchor.Idl, programId, provider!),
+    [provider, programId, idl]
+  );
 
   return program;
 }
